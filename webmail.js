8559f28107ade478933f902f50878b1db20fd848
Ankit Solanki <ankit03june@gmail.com>
2018-04-15 04:15:17 +0530

    working version webmail.js

--- webmail.js
+++ webmail.js
@@ -3,6 +3,8 @@ console.log("[*][*] Autofill Moodle Running...")
 var n = text.search("\n--\n");
 var str = text.substring(n, n+120);
 console.log(str)
+var captcha="";
+var ans = 0;
 
 var array = [];
 
@@ -42,7 +44,11 @@ for(var i=0; i<s.length; i++){
 		s[i] = s[i].slice(0,-1);
 	}
 }
-
+for(var i=0; i<s.length; i++){
+	if(s[i].slice(-1)==";"){
+		s[i] = s[i].slice(0,-1);
+	}
+}
 for(var i=0; i<s.length; i++){
 	if(s[i].search("chars before")!=-1){
 		var spl = s[i].split(" ");
@@ -51,43 +57,81 @@ for(var i=0; i<s.length; i++){
 			{
 				spl[4] = spl[4].slice(0,-1);
 			}
-			console.log(String.fromCharCode(spl[4].charCodeAt(0) - parseInt(spl[0]) + 32));
+			var v = (String.fromCharCode(spl[4].charCodeAt(0) - parseInt(spl[0]) + 32));
+			captcha = captcha.concat(v);
 		}
 		else{
 			if (spl[3].slice(-1)==";")
 			{
 				spl[3] = spl[3].slice(0,-1);
 			}
-			console.log(String.fromCharCode(spl[3].charCodeAt(0) - parseInt(spl[0])));
+			var v = (String.fromCharCode(spl[3].charCodeAt(0) - parseInt(spl[0])));
+			captcha = captcha.concat(v);		
 		}
 	}
 	else if(s[i].slice(-1)=='_' && s[i].slice(0,1)=='_'){
-		console.log(s[i].slice(1,2));
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
 	}
 	else if(s[i].slice(-1)=='"'&& s[i].slice(0,1)=='"'){
-		console.log(s[i].slice(1,2));
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
 	}
 	else if(s[i].slice(-1)=='-'&& s[i].slice(0,1)=='-'){
-		console.log(s[i].slice(1,2));
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
 	}
 	else if(s[i].slice(-1)==':'&& s[i].slice(0,1)==':'){
-		console.log(s[i].slice(1,2));
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
 	}
 	else if(s[i].slice(-1)=="'"&& s[i].slice(0,1)=="'"){
-		console.log(s[i].slice(1,2));
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
+	}
+	else if(s[i].slice(-1)=='/'&& s[i].slice(0,1)=='/'){
+		var v = (s[i].slice(1,2));
+		captcha = captcha.concat(v);
 	}
 	else if(s[i].search("letter")!=-1){
 		if(s[i].search("small")!=-1){
 			var spl = s[i].split(" ");
 			var num = spl[2].slice(1);
 			var char = spl[1].slice(1,2);
-			console.log(String.fromCharCode(char.charCodeAt(0) + parseInt(num)));
+			var v = (String.fromCharCode(char.charCodeAt(0) + parseInt(num) + 32));
+			captcha = captcha.concat(v);
 		}
 		else{
 			var spl = s[i].split(" ");
 			var num = spl[1].slice(1);
 			var char = spl[0].slice(1,2);
-			console.log(String.fromCharCode(char.charCodeAt(0) + parseInt(num) + 32));
+			var v = (String.fromCharCode(char.charCodeAt(0) + parseInt(num)));
+			captcha = captcha.concat(v);
 		}
 	}
+	else if(s[i].search(" = ")!=-1){
+		s[i] = s[i].replace('(','');
+		s[i] = s[i].replace(')','');
+		var spl = s[i].split(" ");
+		ans = parseInt(spl[spl.length-1]);
+		for(var i=0; i < spl.length-3; i=i+2){
+			if(spl[i+1]="-"){
+				ans = ans + parseInt(spl[i+2]);
+			}else if(spl[i+1]="+"){
+				ans = ans - parseInt(spl[i+2]);
+			}else if(spl[i+1]="*"){
+				ans = ans/parseInt(spl[i+2]);
+			}else if(spl[i+1]="/"){
+				ans = ans*parseInt(spl[i+2]);
+			}
+		}
+		var v = (ans);
+		captcha = captcha.concat(v);
+	}
 }
+
+
+var delay = 500;
+setTimeout(function() {
+ document.getElementsByName("captcha_input")[0].value = captcha;
+}, delay);
